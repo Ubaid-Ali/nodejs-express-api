8404f8909ea3087f63d429a289e75d167396fc36
Ubaid Ali <syedubaidali2020@gmail.com>
2022-01-08 17:48:57 +0500

    add user.js with controller folder
    Make the logic functions in separate folder

--- routes/users.js
+++ routes/users.js
@@ -1,59 +1,24 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
+import {
+  getUsers,
+  createUser,
+  getUser,
+  deleteUser,
+  updateUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
-let users = [];
-
 // all routes in here are starting with /users
 
-router.get("/", (req, res) => {
-  res.send(users);
-});
+router.get("/", getUsers);
 
-router.post("/", (req, res) => {
-  const user = req.body;
-  if (user.firstName && user.lastName && user.age) {
-    users.push({ ...user, id: uuidv4() });
-    res.send(
-      `User with the name ${user.firstName} added to the database Successfully!`
-    );
-  } else {
-    res.send(`:( Operation failed while adding user to database!`);
-  }
-});
+router.post("/", createUser);
 
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const userFound = users.find((user) => user.id === id);
-  if (userFound) {
-    res.send(userFound);
-  } else {
-    res.send(`This user id ${id} does not exist`);
-  }
-});
+router.get("/:id", getUser);
 
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  const userFound = users.find((user) => user.id === id);
-  if (userFound) {
-    users = users.filter((user) => user.id !== id);
-    res.send(`User with the id ${id} deleted from the database successfully`);
-  } else {
-    res.send(`This user id ${id} does not exist`);
-  }
-});
+router.delete("/:id", deleteUser);
 
-router.patch("/:id", (req, res) => {
-  const { id } = req.params;
-  const { firstName, lastName, age } = req.body;
-
-  const user = users.find((user) => user.id === id);
-  if (firstName) user.firstName = firstName;
-  if (lastName) user.lastName = lastName;
-  if (age) user.age = age;
-
-  res.send(`User with the id ${id} has been updated`);
-});
+router.patch("/:id", updateUser);
 
 export default router;
